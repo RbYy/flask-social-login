23c653c3f0d4a0663c54a0cab6b7848e5e7b6211
RbYy <robert.brumat@gmail.com>
2016-11-17 17:50:30 +0100

    implement without flywheel

--- dynamo.py
+++ dynamo.py
@@ -1,4 +1,3 @@
-from flywheel import Model, Field, Engine
 from flask import Flask, redirect, url_for, render_template, flash, request, session
 from flask_login import LoginManager, UserMixin, login_user, logout_user,\
     current_user
@@ -15,27 +14,35 @@ app.config.from_pyfile('config.ini', silent=True)
 lm = LoginManager(app)
 lm.login_view = 'index'
 
-engine = Engine()
-engine.connect_to_region('eu-west-1')
 
-
-class GUIDUserMixin(UserMixin):
-    def get_id(self):
-        self.id = self.GUID
-        return super(GUIDUserMixin, self).get_id()
-
-
-class RobertBrumatTest(GUIDUserMixin, Model):
+class User(UserMixin):
     __dynamoDB = boto3.resource('dynamodb')
     __T = __dynamoDB.Table('RobertBrumatTest')
-    GUID = Field(hash_key=True)
-    password = Field()
 
-    def saveLogin(self, social_data, provider):
+    def __init__(self):
+        self.item = {}
+
+    def get_id(self):
+        self.id = self.item['GUID']
+        return super(User, self).get_id()
+
+    def getUser(self, GUID, provider='user'):
+        ts = datetime.now()
+        # res = self.__T.get_item(Key={'guid': 'smn::%s::%s' % (provider, id)})
+        res = self.__T.get_item(Key={'GUID': GUID})
+        if 'Item' in res:
+            self.item = res['Item']
+            return self
+        self.executionTime = datetime.now() - ts
+        return None
+
+    def save(self, social_data, provider):
         ts = datetime.now()
 
         storeArray = social_data
-        if provider == 'google':
+        if provider == 'user':
+            storeArray['GUID'] = 'smn::%s::%s' % (provider, social_data['username'])
+        elif provider == 'google':
             storeArray['GUID'] = 'smn::%s::%s' % (provider, social_data['sub'])
         else:
             storeArray['GUID'] = 'smn::%s::%s' % (provider, social_data['id'])
@@ -44,20 +51,17 @@ class RobertBrumatTest(GUIDUserMixin, Model):
             if value == '':
                 storeArray[key] = None
 
-        RobertBrumatTest.__T.put_item(Item=storeArray)
-        self.post_save_()
+        self.__class__.__T.put_item(Item=storeArray)
+        self.item = storeArray
         self.executionTime = datetime.now() - ts
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
 
-engine.register(RobertBrumatTest)
-
-
 @lm.user_loader
 def load_user(id):
-    return engine.query(RobertBrumatTest).filter(GUID=id).first(desc=True)
+    return User().getUser(id)
 
 
 @app.route('/')
@@ -78,19 +82,16 @@ def generate_GUID():
 
 @app.route('/register', methods=['POST'])
 def register():
-    exists = engine.query(RobertBrumatTest).filter(GUID=generate_GUID()).first(desc=True)
-
-    if exists:
+    user = User()
+    if user.getUser(generate_GUID()):
         flash('Username already taken')
         return redirect(url_for('index'))
 
-    password = generate_password_hash(request.form['password'])
-    u = RobertBrumatTest(GUID=generate_GUID())
-    u.email = request.form['email']
-    u.password = password
-    engine.save(u)
-    login_user(u, True)
-    session['me'] = {'GUID:': u.GUID}
+    registration_data = {key: value for key, value in request.form.iteritems() if key != 'password'}
+    registration_data['password'] = generate_password_hash(request.form['password'])
+    user.save(registration_data, 'user')
+    session['me'] = registration_data
+    login_user(user, True)
     flash('User successfully registered')
     return redirect(url_for('index'))
 
@@ -101,16 +102,17 @@ def login():
     remember_me = False
     if 'remember_me' in request.form:
         remember_me = True
-    exists = engine.query(RobertBrumatTest).filter(GUID=generate_GUID()).first(desc=True)
-    if not exists:
+    user = User().getUser(generate_GUID())
+    if not user:
         flash('Username is invalid')
         return redirect(url_for('index'))
 
-    if not check_password_hash(exists.password, password):
+    if not check_password_hash(user.item['password'], password):
         flash('Password is invalid')
         return redirect(url_for('index'))
-    session['me'] = {'username:': exists.GUID}
-    login_user(exists, remember=remember_me)
+    print('email: ', user.item['email'])
+    session['me'] = {'username:': user.item['GUID']}
+    login_user(user, remember=remember_me)
     flash('Logged in successfully')
     return redirect(request.args.get('next') or url_for('index'))
 
@@ -137,25 +139,15 @@ def oauth_callback(provider):
     print('ffffff', social_id, provider)
     # user_data, GUID = db.getUser(social_id, provider)
     # print('blabla', GUID)
-    user = engine.query(RobertBrumatTest).filter(GUID=social_id).first(desc=True)
-
+    # user = engine.query(RobertBrumatTest).filter(GUID=social_id).first(desc=True)User
+    user = User().getUser(social_id)
     if not user:  # create new user with provider's data and a placeholder for password
         print('creating user ...')
-        user = RobertBrumatTest(social_id)
-        user.saveLogin(social_data, provider)
+        user = User().saveLogin(social_data, provider)
     login_user(user, True)
     session['me'] = social_data
     return redirect(url_for('index'))
 
 
-app.run(debug=True)
-
-# Register our model with the engine so it can create the Dynamo table
-
-# engine.create_schema()
-# social = RobertBrumatTest('smn::facebook::123456789')
-# engine.save(social)
-
-
-# print(item.GUID)
-# item.refresh()
+if __name__ == '__main__':
+    app.run(debug=True)
